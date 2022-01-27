b612a77951621d1b382a828accd9eda7aa5500c1
thiagosquid <thiagofg.eng@gmail.com>
2022-01-27 13:57:42 -0300

    new cors configuration to test

--- src/main/java/com/turma20211/mural/security/JWTConfiguration.java
+++ src/main/java/com/turma20211/mural/security/JWTConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class JWTConfiguration extends WebSecurityConfigurerAdapter {
