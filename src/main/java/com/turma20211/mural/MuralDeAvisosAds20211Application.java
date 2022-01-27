0bdfc088f7f2270a89093b32fcc9e6f01185558a
thiagosquid <thiagofg.eng@gmail.com>
2022-01-27 13:42:16 -0300

    new cors configuration to test

--- src/main/java/com/turma20211/mural/MuralDeAvisosAds20211Application.java
+++ src/main/java/com/turma20211/mural/MuralDeAvisosAds20211Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MuralDeAvisosAds20211Application {
