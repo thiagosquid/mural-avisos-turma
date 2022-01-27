a996f5058908c3ed7cdbba667e671f716ff4a7b9
thiagosquid <thiagofg.eng@gmail.com>
2022-01-27 14:14:10 -0300

    new cors configuration to test

--- src/main/java/com/turma20211/mural/config/CorsConfiguration.java
+++ src/main/java/com/turma20211/mural/config/CorsConfiguration.java
@@ -3,6 +3,7 @@ package com.turma20211.mural.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
