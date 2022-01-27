4491fb51e00f5bfe0652eed1aa89e1f01e608e31
thiagosquid <thiagofg.eng@gmail.com>
2022-01-27 13:39:56 -0300

    new cors configuration to test

--- src/main/java/com/turma20211/mural/MuralDeAvisosAds20211Application.java
+++ src/main/java/com/turma20211/mural/MuralDeAvisosAds20211Application.java
@@ -18,5 +18,17 @@ public class MuralDeAvisosAds20211Application {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder;
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+			}
+		};
+	}
 
 }
